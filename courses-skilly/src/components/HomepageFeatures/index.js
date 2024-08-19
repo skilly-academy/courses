aca0f5d6e060f9d1e4767834ad777b1eef05fd1a
Nuria Liaño <lianonuria@gmail.com>
2024-08-19 11:40:24 +0200

    add courses structure

---
+++ courses-skilly/src/components/HomepageFeatures/index.js
@@ -0,0 +1,79 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Fundamentos',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+    enlace: '/docs/category/fundamentos',
+  },
+  {
+    title: 'Programación',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+    enlace: '/docs/category/programacion', // Añade un enlace aquí también si lo necesitas
+  },
+  {
+    title: 'Bases de datos',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+    enlace: '/docs/category/bases_datos', // Añade un enlace aquí también si lo necesitas
+  },
+  {
+    title: 'Administración de sistemas',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+    enlace: '/docs/category/sistemas', // Añade un enlace aquí también si lo necesitas
+  },
+];
+
+function Feature({Svg, title, description, enlace}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+        <a href={enlace} className="button button--primary">Ver más</a>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
